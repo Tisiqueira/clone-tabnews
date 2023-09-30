66471c51cb6b4f4470e2d2b0946278c712f4af94
Tiago Siqueira <tiago.m.siqueira@outlook.com>
2023-09-30 19:03:10 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do https://www.tabnews.com.br para estudo do curso https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-function Home (){
-    return(
-        <h1>Joyce se vc me ama da uma risadinha ai vai kkkkk.</h1>
-    )
+function Home() {
+  return <h1>Joyce se vc me ama da uma risadinha ai vai kkkkk.</h1>;
 }
 
 export default Home;
