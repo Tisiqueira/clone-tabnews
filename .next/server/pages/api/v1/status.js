3c5cd7a04ee90702551bfaffe0db586db291e17f
Tiago Siqueira <tiago.m.siqueira@outlook.com>
2023-10-20 03:28:36 +0000

    add `/api/v1/status` endpoint

---
+++ .next/server/pages/api/v1/status.js
@@ -0,0 +1,36 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/api/v1/status";
+exports.ids = ["pages/api/v1/status"];
+exports.modules = {
+
+/***/ "(api)/./pages/api/v1/status/index.js":
+/*!**************************************!*\
+  !*** ./pages/api/v1/status/index.js ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction status(request, response) {\n    response.status(200).json({\n        chave: \"valor da m\\xe9dia\"\n    });\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (status);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwaSkvLi9wYWdlcy9hcGkvdjEvc3RhdHVzL2luZGV4LmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSxTQUFTQSxPQUFPQyxPQUFPLEVBQUVDLFFBQVEsRUFBRTtJQUNqQ0EsU0FBU0YsTUFBTSxDQUFDLEtBQUtHLElBQUksQ0FBQztRQUFFQyxPQUFPO0lBQWlCO0FBQ3REO0FBRUEsaUVBQWVKLE1BQU1BLEVBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9jbG9uZS10YWJuZXdzLy4vcGFnZXMvYXBpL3YxL3N0YXR1cy9pbmRleC5qcz84NGRiIl0sInNvdXJjZXNDb250ZW50IjpbImZ1bmN0aW9uIHN0YXR1cyhyZXF1ZXN0LCByZXNwb25zZSkge1xuICByZXNwb25zZS5zdGF0dXMoMjAwKS5qc29uKHsgY2hhdmU6IFwidmFsb3IgZGEgbcOpZGlhXCIgfSk7XG59XG5cbmV4cG9ydCBkZWZhdWx0IHN0YXR1cztcbiJdLCJuYW1lcyI6WyJzdGF0dXMiLCJyZXF1ZXN0IiwicmVzcG9uc2UiLCJqc29uIiwiY2hhdmUiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(api)/./pages/api/v1/status/index.js\n");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("(api)/./pages/api/v1/status/index.js"));
+module.exports = __webpack_exports__;
+
+})();
